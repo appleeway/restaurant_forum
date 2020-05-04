3b0c3c91f35559f43d73edff16f5bd619e751e75
Appleeway <applee62002@gmail.com>
2020-05-04 21:52:19 +0800

    Add putCategory controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -18,14 +18,30 @@ const categoryController = {
 
     })
   },
-
   postCategory: (req, res) => {
     return Category.create({
       name: req.body.name
     }).then((category) => {
+      req.flash('success_messages', 'category was successfully created')
       return res.redirect('/admin/categories')
     })
+  },
+  putCategory: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+
+    return Category.findByPk(req.params.id).then((category) => {
+      category.update({
+        name: req.body.name
+      }).then((category) => {
+        req.flash('success_messages', 'category was successfully update')
+        return res.redirect('/admin/categories')
+      })
+    })
   }
+
 }
 
 module.exports = categoryController
