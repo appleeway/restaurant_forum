78b6c5ce15c7157d3acbcbe7e41299a0a0d02703
Appleeway <applee62002@gmail.com>
2020-05-04 22:12:52 +0800

    Add text empty check on postCategory controller

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -19,12 +19,17 @@ const categoryController = {
     })
   },
   postCategory: (req, res) => {
-    return Category.create({
-      name: req.body.name
-    }).then((category) => {
-      req.flash('success_messages', 'category was successfully created')
-      return res.redirect('/admin/categories')
-    })
+    if (!req.body.name) {
+      req.flash('error_messages', 'name didn\'t exist')
+      return res.redirect('back')
+    } else {
+      return Category.create({
+        name: req.body.name
+      }).then((category) => {
+        req.flash('success_messages', 'category was successfully created')
+        return res.redirect('/admin/categories')
+      })
+    }
   },
   putCategory: (req, res) => {
     if (!req.body.name) {
