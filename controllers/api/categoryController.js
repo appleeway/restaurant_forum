04bf8eb4c70f5f114038a689838519cf057e6388
Appleeway <applee62002@gmail.com>
2020-05-11 23:10:13 +0800

    Refactor GET /api/admin/categories

--- controllers/api/categoryController.js
+++ controllers/api/categoryController.js
@@ -1,1 +1,12 @@
 // controllers/api/catrgoryController.js
+const categoryService = require('../../services/categoryService')
+
+const categoryController = {
+  getCategories: (req, res) => {
+    categoryService.getCategories(req, res, (data) => {
+      return res.json(data)
+    })
+  }
+}
+
+module.exports = categoryController
--- services/categoryService.js
+++ services/categoryService.js
@@ -1,1 +1,20 @@
 // services/categoryService.js
+
+const db = require('../models')
+const Category = db.Category
+
+const categoryController = {
+  getCategories: (req, res, cb) => {
+    Category.findAll().then((categories) => {
+      if (req.params.id) {
+        Category.findByPk(req.params.id).then((category) => {
+          cb({ categories: categories, category: category })
+        })
+      } else {
+        cb({ categories: categories })
+      }
+    })
+  },
+
+}
+module.exports = categoryController
