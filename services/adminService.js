4878659d3f2aa2d723512666c1e9e03e06bb2c22
Appleeway <applee62002@gmail.com>
2020-05-11 19:57:35 +0800

    refactor adminController.getRestaurants

---
+++ services/adminService.js
@@ -0,0 +1,13 @@
+// services/adminService.js
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
+const adminService = {
+  getRestaurants: (req, res, cb) => {
+    return Restaurant.findAll({ include: [Category] }).then(restaurants => {
+      cb({ restaurants: restaurants })
+    })
+  },
+}
+module.exports = adminService
